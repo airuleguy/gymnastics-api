9fbad2194f279bc82b96f7c49ebdb2f3b251836b
Alejandro Iruleguy <ale.iruleguy@gmail.com>
2022-08-20 12:30:10 -0300

    feat(db): correct setup for DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { configuration } from './config/config';
+import { DatabaseConfig } from './config/database.config';
 
 import { GymnastModule } from './gymnasts/gymnasts.module';
 import { TournamentModule } from './tournament/tournament.module';
 
 @Module({
-  imports: [GymnastModule, TournamentModule, TypeOrmModule],
+  imports: [
+    GymnastModule,
+    TournamentModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: ['.env', '.local.env', '.production.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
