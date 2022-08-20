b17be1c915389310b24e0d3e3adf53b46098d195
Alejandro Iruleguy <ale.iruleguy@gmail.com>
2022-08-20 12:29:52 -0300

    feat(club): federation type of each club now available

--- src/clubs/clubs.entity.ts
+++ src/clubs/clubs.entity.ts
@@ -1,4 +1,6 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { FederationTypes } from 'src/federation_types/federation_types.enum';
+import { Gymnast } from 'src/gymnasts/gymnasts.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({
   name: 'clubs',
@@ -6,4 +8,30 @@ import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class Club {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({
+    type: String,
+    nullable: false,
+    length: 50,
+  })
+  name: string;
+
+  @Column({
+    type: String,
+    nullable: true,
+    length: 5,
+  })
+  code: string;
+
+  @Column({
+    type: 'enum',
+    enum: FederationTypes,
+    nullable: false,
+  })
+  federationType: FederationTypes;
+
+  @OneToMany(() => Gymnast, (gymnast: Gymnast) => gymnast.id, {
+    cascade: false,
+  })
+  gymnasts: Gymnast[];
 }
---
+++ src/federation_types/federation_types.enum.ts
@@ -0,0 +1,4 @@
+export enum FederationTypes {
+  A = 'a',
+  B = 'b',
+}
