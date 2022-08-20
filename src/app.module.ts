b4c52413f539b0880d873bae23081d66b8ebd882
Alejandro Iruleguy <ale.iruleguy@gmail.com>
2022-08-20 11:22:54 -0300

    feat(typeorm): changed dynamo for postgre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GymnastModule } from './gymnast/gymnast.module';
+
+import { GymnastModule } from './gymnasts/gymnasts.module';
 import { TournamentModule } from './tournament/tournament.module';
 
 @Module({
-  imports: [GymnastModule, TournamentModule],
+  imports: [GymnastModule, TournamentModule, TypeOrmModule],
   controllers: [AppController],
   providers: [AppService],
 })
