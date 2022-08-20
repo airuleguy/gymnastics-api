9f3fb120df5fc0b2b725da36b7cb86f00648ae45
Alejandro Iruleguy <ale.iruleguy@gmail.com>
2022-08-20 11:24:00 -0300

    feature(db config): created files to be able to configure differently depending on environment

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+export const configuration = () => ({
+  database: {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    logging: process.env.POSTGRES_LOGGING,
+    synchronize: true,
+    autoLoadEntities: true,
+  },
+});
