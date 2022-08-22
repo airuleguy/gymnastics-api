f3a27504f6846120ea5e84b5b34248d4711ee4ea
Alejandro Iruleguy <ale.iruleguy@gmail.com>
2022-08-22 19:19:56 -0300

    feat(exceptions): global handler for TypeORM exceptions going up

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { TypeORMExceptionFilter } from './exceptions/filters/exceptions.filters.db';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -14,6 +15,7 @@ async function bootstrap() {
   );
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new TypeORMExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Gymnasts API')
